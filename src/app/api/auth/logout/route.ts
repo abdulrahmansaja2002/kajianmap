a37b7e1d4885e8bd9d7d10c800e55ae26b102a39
Abdul Rahman <abdul.rahman.saja2002@gmail.com>
2026-09-05 17:26:08 +0700

    fix: pass req params in handler since it used for retriving cookie

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,5 +1,6 @@
 import { AuthHandler } from "@/server/handler/auth.handler";
+import type { NextRequest } from "next/server";
 
-export async function POST() {
-  return AuthHandler.logout();
+export async function POST(req: NextRequest) {
+  return AuthHandler.logout(req);
 }
